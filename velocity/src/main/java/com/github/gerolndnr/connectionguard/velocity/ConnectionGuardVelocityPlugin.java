9f6a473c8f24712434cd839c94f0fee9141fd9f1
Gero Lindner <gero.lindner@protonmail.com>
2024-12-29 21:46:44 +0100

    Updated version to 0.4.3

--- velocity/src/main/java/com/github/gerolndnr/connectionguard/velocity/ConnectionGuardVelocityPlugin.java
+++ velocity/src/main/java/com/github/gerolndnr/connectionguard/velocity/ConnectionGuardVelocityPlugin.java
@@ -30,7 +30,7 @@ import java.util.HashMap;
 @Plugin(
         id="connection-guard",
         name="Connection Guard",
-        version="0.4.2",
+        version="0.4.3",
         url="https://github.com/gerolndnr/connection-guard",
         authors = {"gerolndnr"}
 )
